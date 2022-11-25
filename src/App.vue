a0fbadb0833bca8045fd2c24c43096dafc451926
Your Name <you@example.com>
2022-11-26 00:33:32 +0700

    Chang all new

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
 <div class="image w-100 vh-100 d-flex align-items-center justify-content-center">
-  <h1>
-    
-  </h1>
+    <div class="text-center">
+        <h2 class="text-white" style="font-size: 48px;">OUR WEBSITE IS..</h2>
+        <h1 class="text-white fw-bold" style="font-size: 82px">COMING SOON</h1>
+      </div>
 </div>
 </template>
 
@@ -13,10 +14,12 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Chakra+Petch:wght@400;700&display=swap');
+
 html,body { padding: 0 !important;}
 
 .image {
-  background-image: url('../src/assets/ComingSoon.png');
+  background-image: url('../src/assets/ComingSoon2.png');
   background-size: cover;
 }
 </style>

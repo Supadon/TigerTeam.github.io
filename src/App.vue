0d2d3fb072a5fda22e818503868dc40ceea66dba
Your Name <you@example.com>
2022-11-26 00:43:41 +0700

    Chang all new

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="image w-100 vh-100 d-flex align-items-center justify-content-center">
     <div class="text-center">
-        <h2 class="text-white" style="font-size: 48px;">OUR WEBSITE IS..</h2>
+        <h2 class="text-white fw-light" style="font-size: 48px;">OUR WEBSITE IS..</h2>
         <h1 class="text-white fw-bold" style="font-size: 82px">COMING SOON</h1>
       </div>
 </div>
@@ -14,7 +14,8 @@ export default {
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Chakra+Petch:wght@400;700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Chakra+Petch:wght@300;700&display=swap');
+
 
 html,body { padding: 0 !important;}
 
